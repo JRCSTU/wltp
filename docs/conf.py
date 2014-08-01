dbc2f2308c0a4b34c8e82f3b0c3bc35fa47d9dbb
ankostis@host:STUW025 <ankostis@gmail.com>
2014-08-01 03:16:17 +0200

    docs: RTD sphinx-style.

--- docs/conf.py
+++ docs/conf.py
@@ -25,6 +25,8 @@ def read_project_version():
     return fglobals['__version__']
 proj_ver = read_project_version()
 
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -115,8 +117,8 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-if os.environ.get('DOCS_LOCAL'):
-    import sphinx_rtd_theme
+if on_rtd:
+    import sphinx_rtd_theme  # @UnresolvedImport
     html_theme = "sphinx_rtd_theme"
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 else:
