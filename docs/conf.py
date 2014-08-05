6578cd1fd83531507c685f9fa82c5ec196e27ee5
ankostis@kilo <ankostis@gmail.com>
2014-08-05 07:05:37 +0200

    docs: conf.py: FIX infinite-recursion after commiting docs-->docs symlink, by excluding it.

--- docs/conf.py
+++ docs/conf.py
@@ -123,7 +123,7 @@ todo_include_todos = True
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['_build', 'docs']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
