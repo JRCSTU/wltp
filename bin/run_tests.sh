e1f643950d64477823d0ed76c6b5d51f21e71266
Kostis Anagnostopoulos @ kilo <ankostis@gmail.com>
2014-11-18 23:54:32 +0100

    docs: Renable matplolib-metrics in RTD. [ci skip]

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -13,9 +13,32 @@
 my_dir=`dirname "$0"`
 cd $my_dir/..
 
+declare -A fails
 if  python -c 'import sys; print(sys.version_info[0])'| grep -q '3'; then
     echo "Python-3: Running ALL tests (code-tests, doctests, coverage)."
-    python setup.py test_all
+    python setup.py doctest_docs    || fails['doctest_docs'] 
+    python setup.py doctest_code    || fails['doctest_code']=$?
+    python setup.py test_code       || fails['test_code']=$?
+    
+    rst="`which rst2html.py`"
+    if [ -n "`which cygpath`" ]; then 
+        rst="`cygpath -w $rst`"
+    fi
+    python setup.py --long-description | python "$rst"  --halt=warning > /dev/null || fails['README']=$?
+    
+    python setup.py build_sphinx    || fails["build_sphinx"]=$?
+    
+    ## Print results
+    #
+    if [ ${#fails[@]} -gt 0 ]; then
+        echo -n "TEST FAILURES:  "
+        for fail in "${!fails[@]}"; do 
+            echo -n "${fail}(${fails[$fail]})"
+        done
+        echo
+        
+        exit 1
+    fi
 else
     echo "Python-2: Running ONLY code-tests."
     python setup.py test_code
