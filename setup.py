493b34596826207a3f83a287003b797ca49a778a
Kostis Anagnostopoulos <ankostis@gmail.com>
2018-08-23 20:44:54 +0200

    fix(doc): enhance RsT-only filter conversions for site RsT

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ def read_text_lines(fname):
         return fd.readlines()
 
 
-def yield_sphinx_only_markup(lines):
+def yield_rst_only_markup(lines):
     """
     :param file_inp:     a `filename` or ``sys.stdin``?
     :param file_out:     a `filename` or ``sys.stdout`?`
@@ -71,35 +71,47 @@ def yield_sphinx_only_markup(lines):
     substs = [
         # Selected Sphinx-only Roles.
         #
-        (r':abbr:`([^`]+)`',        r'\1'),
-        (r':ref:`([^`]+)`',         r'ref:`\1`_'),
-        (r':term:`([^`]+)`',        r'**\1**'),
-        (r':dfn:`([^`]+)`',         r'**\1**'),
-        (r':(samp|guilabel|menuselection):`([^`]+)`',        r'``\2``'),
-
+        (r':abbr:`([^`]+)`', r'\1'),
+        (r':ref:`([^`]+)`', r'ref: *\1*'),
+        (r':term:`([^`]+)`', r'**\1**'),
+        (r':dfn:`([^`]+)`', r'**\1**'),
+        (r':(samp|guilabel|menuselection|doc|file):`([^`]+)`', r'``\2``'),
 
         # Sphinx-only roles:
         #        :foo:`bar`   --> foo(``bar``)
         #        :a:foo:`bar` XXX afoo(``bar``)
         #
         #(r'(:(\w+))?:(\w+):`([^`]*)`', r'\2\3(``\4``)'),
-        (r':(\w+):`([^`]*)`', r'\1(`\2`)'),
+        #(r':(\w+):`([^`]*)`', r'\1(`\2`)'),
+        # emphasis
+        # literal
+        # code
+        # math
+        # pep-reference
+        # rfc-reference
+        # strong
+        # subscript, sub
+        # superscript, sup
+        # title-reference
 
 
         # Sphinx-only Directives.
         #
-        (r'\.\. doctest',           r'code-block'),
-        (r'\.\. plot::',            r'.. '),
-        (r'\.\. seealso',           r'info'),
-        (r'\.\. glossary',          r'rubric'),
-        (r'\.\. figure::',          r'.. '),
-        (r'\.\. image::',          r'.. '),
+        (r'\.\. doctest', r'code-block'),
+        (r'\.\. module', r'code-block'),
+        (r'\.\. plot::', r'.. '),
+        (r'\.\. seealso', r'info'),
+        (r'\.\. glossary', r'rubric'),
+        (r'\.\. figure::', r'.. '),
+        (r'\.\. image::', r'.. '),
 
+        (r'\.\. dispatcher', r'code-block'),
 
         # Other
         #
-        (r'\|version\|',              r'x.x.x'),
-        (r'\|today\|',                r'YYYY-MM-DD'),
+        (r'\|version\|', r'x.x.x'),
+        (r'\|today\|', r'x.x.x'),
+        (r'\.\. include:: AUTHORS', r'see: AUTHORS'),
     ]
 
     regex_subs = [(re.compile(regex, re.IGNORECASE), sub)
@@ -124,7 +136,7 @@ proj_ver = read_project_version()
 
 readme_lines = read_text_lines('README.rst')
 description = readme_lines[1]
-long_desc = ''.join(yield_sphinx_only_markup(readme_lines))
+long_desc = ''.join(yield_rst_only_markup(readme_lines))
 # Trick from: http://peterdowns.com/posts/first-time-with-pypi.html
 download_url = 'https://github.com/ankostis/%s/tarball/v%s' % (
     proj_name, proj_ver)
