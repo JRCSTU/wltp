adb2469e2606698a6fa5a7350d99785894831871
Kostis Anagnostopoulos <ankostis@gmail.com>
2018-08-23 20:52:35 +0200

    chore(ver): bump 10.1.0a2-->0.1.0aA3

--- wltp/_version.py
+++ wltp/_version.py
@@ -1,3 +1,3 @@
 """Authoritative project's version-info"""
-__version__ = "0.1.0-alpha.2"          ## Semantic-version: Update identifiers also in README.rst.
-__updated__ = "2017-05-25 18:39:47"
+__version__ = "0.1.0a3"          ## Semantic-version: Update identifiers also in README.rst.
+__updated__ = "2018-08-23 20:39:47"
