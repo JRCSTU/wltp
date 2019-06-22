7be009dbd0ea672dd006fc67508e50acea5a28f3
Kostis Anagnostopoulos <ankostis@gmail.com>
2019-06-22 15:25:49 +0200

    fix(build): clean build/ idocs/biuld/ before ./bin/run-tests.sh

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -7,6 +7,10 @@ my_dir=`dirname "$0"`
 cd $my_dir/..
 
 declare -A fails
+
+# Pytest fails sometimes with old cache from git.
+rm -rf build/* docs/_build/*
+
 if  python -c 'import sys; print(sys.version_info[0:2])'| grep -q '(3, 7)'; then
     echo "Python-3: Running ALL tests (code-tests, doctests, coverage)."
 
