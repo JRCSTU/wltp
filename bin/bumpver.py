db520f854c74429bbee3d7eb1847484e22a2cbad
Kostis Anagnostopoulos <ankostis@gmail.com>
2019-07-31 15:36:55 +0300

    style(build): blacken `bumpver.py`

--- bin/bumpver.py
+++ bin/bumpver.py
@@ -26,11 +26,11 @@ import docopt
 
 my_dir = osp.dirname(__file__)
 
-VFILE = osp.join(my_dir, '..', 'wltp', '_version.py')
+VFILE = osp.join(my_dir, "..", "wltp", "_version.py")
 VFILE_regex_v = re.compile(r'__version__ = "([^"]+)"')
 VFILE_regex_d = re.compile(r'__updated__ = "([^"]+)"')
 
-RFILE = osp.join(my_dir, '..', 'README.rst')
+RFILE = osp.join(my_dir, "..", "README.rst")
 
 
 class CmdException(Exception):
@@ -39,7 +39,7 @@ class CmdException(Exception):
 
 @fnt.lru_cache()
 def read_txtfile(fpath):
-    with open(fpath, 'rt', encoding='utf-8') as fp:
+    with open(fpath, "rt", encoding="utf-8") as fp:
         return fp.read()
 
 
@@ -54,8 +54,10 @@ def extract_file_regexes(fpath, regexes):
     matches = [regex.search(txt) for regex in regexes]
 
     if not all(matches):
-        raise CmdException("Failed extracting current version: "
-                           "\n  ver: %s\n  date: %s" % tuple(matches))
+        raise CmdException(
+            "Failed extracting current version: "
+            "\n  ver: %s\n  date: %s" % tuple(matches)
+        )
 
     return [m.group(1) for m in matches]
 
@@ -74,8 +76,7 @@ def replace_substrings(files, subst_pairs):
 
 def format_syscmd(cmd):
     if isinstance(cmd, (list, tuple)):
-        cmd = ' '.join('"%s"' % s if ' ' in s else s
-                       for s in cmd)
+        cmd = " ".join('"%s"' % s if " " in s else s for s in cmd)
     else:
         assert isinstance(cmd, str), cmd
 
@@ -85,7 +86,7 @@ def format_syscmd(cmd):
 def strip_ver2_commonprefix(ver1, ver2):
     cprefix = osp.commonprefix([ver1, ver2])
     if cprefix:
-        striplen = cprefix.rfind('.')
+        striplen = cprefix.rfind(".")
         if striplen > 0:
             striplen += 1
         else:
@@ -100,7 +101,7 @@ def run_testcases():
 
     ## Needed to import TCs AFTER grafting of version.
     #
-    sys.path.append(osp.normpath(osp.join(my_dir, '..')))
+    sys.path.append(osp.normpath(osp.join(my_dir, "..")))
     import tests.test_docs as test_docs  # @IgnorePep8
 
     suite = unittest.TestLoader().loadTestsFromModule(test_docs)
@@ -122,13 +123,10 @@ def do_commit(new_ver, old_ver, dry_run, ver_files):
     import pathlib
 
     new_ver = strip_ver2_commonprefix(old_ver, new_ver)
-    cmt_msg = 'chore(ver): bump %s-->%s' % (old_ver, new_ver)
+    cmt_msg = "chore(ver): bump %s-->%s" % (old_ver, new_ver)
 
     ver_files = [pathlib.Path(f).as_posix() for f in ver_files]
-    commands = [
-        ['git', 'add'] + ver_files,
-        ['git', 'commit', '-m', cmt_msg]
-    ]
+    commands = [["git", "add"] + ver_files, ["git", "commit", "-m", cmt_msg]]
 
     for cmd in commands:
         cmd_str = format_syscmd(cmd)
@@ -140,7 +138,7 @@ def do_commit(new_ver, old_ver, dry_run, ver_files):
 
 
 def do_tag(tag, tag_msg, dry_run):
-    cmd = ['git', 'tag', tag, '-s', '-m', tag_msg]
+    cmd = ["git", "tag", tag, "-s", "-m", tag_msg]
     cmd_str = format_syscmd(cmd)
     if dry_run:
         yield "DRYRUN: %s" % cmd_str
@@ -172,7 +170,7 @@ def bumpver(new_ver, dry_run=False, force=False, tag_after_commit=None):
 
         from datetime import datetime
 
-        new_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S%z')
+        new_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S%z")
 
         ver_files = [osp.normpath(f) for f in [VFILE, RFILE]]
         subst_pairs = [(old_ver, new_ver), (old_date, new_date)]
@@ -181,12 +179,12 @@ def bumpver(new_ver, dry_run=False, force=False, tag_after_commit=None):
             new_txt, fpath, replacements = repl
 
             if not dry_run:
-                with open(fpath, 'wt', encoding='utf-8') as fp:
+                with open(fpath, "wt", encoding="utf-8") as fp:
                     fp.write(new_txt)
 
-            yield '%s: ' % fpath
+            yield "%s: " % fpath
             for old, new, nrepl in replacements:
-                yield '  %i x (%24s --> %s)' % (nrepl, old, new)
+                yield "  %i x (%24s --> %s)" % (nrepl, old, new)
 
         yield "...now launching DocTCs..."
         run_testcases()
@@ -195,19 +193,19 @@ def bumpver(new_ver, dry_run=False, force=False, tag_after_commit=None):
             yield from do_commit(new_ver, old_ver, dry_run, ver_files)
 
             if isinstance(tag_after_commit, str):
-                tag = 'v%s' % new_ver
+                tag = "v%s" % new_ver
                 yield from do_tag(tag, tag_after_commit, dry_run)
 
 
 def main(*args):
     opts = docopt.docopt(__doc__, argv=args)
 
-    new_ver = opts['<new-ver>']
-    dry_run = opts['--dry-run']
-    force = opts['--force']
+    new_ver = opts["<new-ver>"]
+    dry_run = opts["--dry-run"]
+    force = opts["--force"]
 
-    commit = opts['--commit']
-    tag = opts['--tag']
+    commit = opts["--commit"]
+    tag = opts["--tag"]
     if tag:
         tag_after_commit = tag
     elif commit:
@@ -224,5 +222,5 @@ def main(*args):
         raise ex
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main(*sys.argv[1:])
