c1c54e476c52202bfddb9e04a0e7c074dc20465a
Kostis Anagnostopoulos <ankostis@gmail.com>
2019-08-05 12:50:21 +0300

    feat(nb.TC): papermill also compare-notebook

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -11,7 +11,7 @@ import pytest
 import papermill as pm
 
 
-@pytest.fixture(params=["CarsDB-pyalgo.ipynb"])
+@pytest.fixture(params=["CarsDB-pyalgo.ipynb", "CarsDB-compare.ipynb"])
 def notebook(request):
     nbfname = request.param
     nbfpath = Path(__file__).parents[1] / "Notebooks" / nbfname
