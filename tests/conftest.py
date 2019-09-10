7d3dfb8754d6bf07c5512af0ddec43f3b5faf3fa
Kostis Anagnostopoulos <ankostis@gmail.com>
2019-09-10 17:14:35 +0300

    FEAT(dtree): generate dtree after TCs have run

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,10 +5,13 @@
 # Licensed under the EUPL (the 'Licence');
 # You may not use this work except in compliance with the Licence.
 # You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+import logging
 from pathlib import Path
 
 import pytest
 from tests import vehdb
+
+from wltp import io as wio
 
 
 def pytest_addoption(parser):
@@ -33,3 +36,19 @@ def h5_accdb():
 @pytest.fixture
 def h5_pyalgo():
     return str(Path("Notebooks/VehData/WltpGS-pyalgo.h5").resolve())
+
+
+@pytest.fixture(scope="session", autouse=True)
+def cleanup(request):
+    """Print used data-psteps once we are finished."""
+
+    def write_data_psteps():
+        pstep_paths = set(
+            i[:-5] if i.endswith("/ndim") else i for i in wio.paths_collected()
+        )
+        dtree_file = Path(__file__).parent.parent.joinpath("datatree.txt")
+        pstep_paths |= set(dtree_file.read_text().split("\n"))
+
+        dtree_file.write_text("\n".join(sorted(pstep_paths)))
+
+    request.addfinalizer(write_data_psteps)
