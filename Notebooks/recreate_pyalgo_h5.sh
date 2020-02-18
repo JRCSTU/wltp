5bd5700992a000934503005b33b806a1bc9d6275
Kostis Anagnostopoulos <ankostis@gmail.com>
2020-02-18 12:47:10 +0200

    FEAT(NBs): COMPARE PyAlgo vs HDF5 seatbelt

--- Notebooks/recreate_pyalgo_h5.sh
+++ Notebooks/recreate_pyalgo_h5.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
 # Run pyalgo on all AccDB cars
-# needed for `CarsDB-compare` notebook. 
+# needed for `CarsDB-compare` notebook.
 
 papermill Notebooks/CarsDB-pyalgo.ipynb /tmp/CarsDB-pyalgo.ipynb \
 	--cwd Notebooks \
     -p skip_h5_write False \
+    -p force_h5_write True \
     -p del_h5_on_start False
-	
