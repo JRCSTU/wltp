2d3c5d3351e21b9c8c0a2d81db1bc625145807bb
Kostis Anagnostopoulos <ankostis@gmail.com>
2020-07-27 21:01:51 +0300

    fix(autog.TC) minor var reuse

--- tests/test_autographed.py
+++ tests/test_autographed.py
@@ -3,8 +3,8 @@ from wltp import autograph, cycles
 
 def test_preserve_ops():
     aug = autograph.Autograph()
-    op = cycles.calc_capped_distances
-    got = aug.wrap_fn(cycles.calc_capped_distances)
+    op = cycles.calc_wltc_distances
+    got = aug.wrap_fn(op)
     assert got == op
     assert got.needs == op.needs
     assert got.provides == op.provides
